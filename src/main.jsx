586332f02b9edce96251b6f10d993a8290e97303
Webprojon <tokhirjonkhasanov@gmail.com>
2024-10-23 19:16:26 +0200

    pwa added

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { registerSW } from "virtual:pwa-register";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
-
-const updateSW = registerSW({
-	onNeedRefresh() {
-		if (confirm("New content available. Reload?")) {
-			window.location.reload();
-		}
-	},
-	onOfflineReady() {
-		console.log("App is ready to work offline");
-	},
-});
-
-console.log(updateSW);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
