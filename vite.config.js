633bc68c456e613a3acda152811242fae18b9197
Webprojon <tokhirjonkhasanov@gmail.com>
2024-10-23 21:26:14 +0200

    added

--- vite.config.js
+++ vite.config.js
@@ -4,46 +4,37 @@ import { VitePWA } from "vite-plugin-pwa";
 
 const manifestForPlugin = {
 	registerType: "prompt",
-	includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.png"],
+	includeAssets: ["favicon.ico", "apple-touc-icon.png", "maskable.png"],
 	manifest: {
-		name: "Weather app",
-		short_name: "Weather app",
-		description: "An app that can show the weather forecast for your city.",
-		icons: [
+		name: "Notes app",
+		short_name: "Notes app",
+		description: "Make you notes",
+		icon: [
 			{
-				src: "./icon-192x192.png",
+				src: "maskable.png",
+				sizes: "196x196",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+			{
+				src: "logo192.png",
 				sizes: "192x192",
 				type: "image/png",
 			},
 			{
-				src: "./icon-512x512.png",
-				sizes: "512x512",
+				src: "logo256.png",
+				sizes: "1256x256",
 				type: "image/png",
-				purpose: "favicon",
 			},
 			{
-				src: "/apple-touch-icon.png",
-				sizes: "180x180",
-				type: "image/png",
-				purpose: "apple touch icon",
-			},
-			{
-				src: "./icon-144x144.png",
-				sizes: "144x144",
-				type: "image/png",
-				purpose: "any",
-			},
-			{
-				src: "./icon-256x256.png",
-				sizes: "256x256",
-				type: "image/png",
-				purpose: "icon",
-			},
-			{
-				src: "./icon-384x384.png",
+				src: "logo384.png",
 				sizes: "384x384",
 				type: "image/png",
-				purpose: "any maskable",
+			},
+			{
+				src: "logo512.png",
+				sizes: "512x512",
+				type: "image/png",
 			},
 		],
 		theme_color: "#181818",
